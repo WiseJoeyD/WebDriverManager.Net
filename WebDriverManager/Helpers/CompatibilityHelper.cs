5b9d95a7e2ccc24ba17e54a601218104ad2b83c9
Joey Khattak <Joey.Khattak@sqs.com>
2020-02-08 19:04:41 +0000

    Update Version String list
    -Use integer and add search command to return version string for browser versions not storing (alows storing version number changes once instead of multiple entries)
    - Add unit tests

--- WebDriverManager/Helpers/CompatibilityHelper.cs
+++ WebDriverManager/Helpers/CompatibilityHelper.cs
@@ -8,7 +8,7 @@ namespace WebDriverManager.Helpers
 {
     static class CompatibilityHelper
     {
-        private static Dictionary<string, string> VersionList;
+        private static Dictionary<int, string> VersionList;
 
         public static void Initialise(BrowserName name)
         {
@@ -91,7 +91,11 @@ namespace WebDriverManager.Helpers
 
             try
             {
-                return VersionList[versionNumber];
+                int.TryParse(versionNumber, out var versionCast);
+
+                var key = VersionList.Keys.Where(a => a <= versionCast).OrderBy(a => a).Last();
+
+                return VersionList[key];
             }
             catch (Exception)
             {
@@ -104,116 +108,112 @@ namespace WebDriverManager.Helpers
 
         private static void SetupInternetExplorerDictionary()
         {
-            VersionList = new Dictionary<string, string>
+            VersionList = new Dictionary<int, string>
             {
                 //Latest version compatible with all previous versions
                 //updated June 2019
                 //3.141.59
-                { "Latest", "3.141.59" }
+                { 1, "3.150.1.0" },
+                { 0, "3.141.59" }
             };
         }
 
         private static void SetupPhantomJSDictionary()
         {
-            VersionList = new Dictionary<string, string>
+            VersionList = new Dictionary<int, string>
             {
                 //2.1.1 - last version
-                { "Latest", "2.1.1" }
+                { 0 , "2.1.1" }
             };
         }
 
         private static void SetupChromeDictionary()
         {
-            VersionList = new Dictionary<string, string>
+            VersionList = new Dictionary<int, string>
             {
                 //updated June 2019
                 // 74+ versions changed version formats
                 //source: https://chromedriver.storage.googleapis.com/index.html
                 //compatbility source: https://chromedriver.storage.googleapis.com/2.46/notes.txt
-                { "73", "2.46" },
-                { "72", "2.46" },
-                { "71", "2.46" },
-                { "70", "2.45" },
-                { "69", "2.44" },
-                { "68", "2.42" },
-                { "67", "2.41" },
-                { "66", "2.40" },
-                { "65", "2.38" },
-                { "64", "2.37" },
-                { "63", "2.36" },
-                { "62", "2.35" },
-                { "61", "2.34" },
-                { "60", "2.33" },
-                { "59", "2.32" },
-                { "58", "2.31" },
-                { "57", "2.29" },
-                { "56", "2.29" },
-                { "55", "2.28" },
-                { "54", "2.27" },
-                { "53", "2.26" },
-                { "52", "2.25" },
-                { "51", "2.23" },
-                { "50", "2.23" },
-                { "49", "2.22" },
-                { "48", "2.21" },
-                { "47", "2.21" },
-                { "46", "2.21" },
-                { "45", "2.20" },
-                { "44", "2.20" },
-                { "43", "2.20" },
-                { "42", "2.17" },
-                { "41", "2.15" },
-                { "40", "2.14" },
-                { "39", "2.14" },
-                { "38", "2.13" },
-                { "37", "2.12" },
-                { "36", "2.12" }
+                { 73, "2.46" },
+                { 72, "2.46" },
+                { 71, "2.46" },
+                { 70, "2.45" },
+                { 69, "2.44" },
+                { 68, "2.42" },
+                { 67, "2.41" },
+                { 66, "2.40" },
+                { 65, "2.38" },
+                { 64, "2.37" },
+                { 63, "2.36" },
+                { 62, "2.35" },
+                { 61, "2.34" },
+                { 60, "2.33" },
+                { 59, "2.32" },
+                { 58, "2.31" },
+                { 56, "2.29" },
+                { 55, "2.28" },
+                { 54, "2.27" },
+                { 53, "2.26" },
+                { 52, "2.25" },
+                { 50, "2.23" },
+                { 49, "2.22" },
+                { 46, "2.21" },
+                { 43, "2.20" },
+                { 42, "2.17" },
+                { 41, "2.15" },
+                { 39, "2.14" },
+                { 38, "2.13" },
+                { 36, "2.12" }
             };
         }
 
         private static void SetupFirefoxDictionary()
         {
-            VersionList = new Dictionary<string, string>
+            VersionList = new Dictionary<int, string>
             {
                 //updated June 2019
                 // Version 57 maintains open ended compatibility
                 //source: https://firefox-source-docs.mozilla.org/testing/geckodriver/Support.html
-                { "57", "0.24.0" },
-                { "56", "0.20.1" },
-                { "55", "0.20.1" },
-                { "54", "0.18.0" },
-                { "53", "0.18.0" },
-                { "52", "0.17.0" }
+                { 60, "0.26.0" },
+                { 57, "0.25.0" },
+                { 55, "0.20.1" },
+                { 54, "0.18.0" },
+                { 53, "0.18.0" },
+                { 52, "0.17.0" }
             };
         }
 
         private static void SetupOperaDictionary()
         {
-            VersionList = new Dictionary<string, string>
+            VersionList = new Dictionary<int, string>
             {
                 //updated March 2019
                 //source: https://github.com/operasoftware/operachromiumdriver/releases
-
-                { "62", "75.0.3770.100"},
-                { "60", "2.45" },
-                { "59", "2.42" },
-                { "58", "2.42" },
-                { "57", "2.41" },
-                { "56", "2.40" },
-                { "55", "2.38" },
-                { "54", "2.37" },
-                { "53", "2.36" },
-                { "52", "2.35" },
-                { "50", "2.33" },
-                { "49", "2.32" },
-                { "48", "2.30" },
-                { "46", "2.29" }
+                { 66, "79.0.3945.79"},
+                { 65, "78.0.3904.87"},
+                { 64, "77.0.3865.120"},
+                { 63, "76.0.3809.132"},
+                { 62, "75.0.3770.100"},
+                { 60, "2.45" },
+                { 59, "2.42" },
+                { 58, "2.42" },
+                { 57, "2.41" },
+                { 56, "2.40" },
+                { 55, "2.38" },
+                { 54, "2.37" },
+                { 53, "2.36" },
+                { 52, "2.35" },
+                { 50, "2.33" },
+                { 49, "2.32" },
+                { 48, "2.30" },
+                { 46, "2.29" }
             };
         }
 
         private static void SetupEdgeDictionary()
         {
-            VersionList = new Dictionary<string, string>
+            VersionList = new Dictionary<int, string>
             {
                 //updated June 2019
                 //source: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
@@ -222,24 +222,24 @@ namespace WebDriverManager.Helpers
                 //Version numbers represent either Edge brower and Edge HTML - due to way Microsoft display both versions
                 //both are shown, so including both to ensure users don't get an error if using either version
 
-                { "42", "F/8/A/F8AF50AB-3C3A-4BC4-8773-DC27B32988DD" },
-                { "17", "F/8/A/F8AF50AB-3C3A-4BC4-8773-DC27B32988DD" },
-                { "41", "D/4/1/D417998A-58EE-4EFE-A7CC-39EF9E020768" },
-                { "16", "D/4/1/D417998A-58EE-4EFE-A7CC-39EF9E020768" },
-                { "40", "3/4/2/342316D7-EBE0-4F10-ABA2-AE8E0CDF36DD" },
-                { "15", "3/4/2/342316D7-EBE0-4F10-ABA2-AE8E0CDF36DD" },
-                { "38", "3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15" },
-                { "14", "3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15" },
-                { "25", "C/0/7/C07EBF21-5305-4EC8-83B1-A6FCC8F93F45" },
-                { "13", "C/0/7/C07EBF21-5305-4EC8-83B1-A6FCC8F93F45" },
-                { "20", "8/D/0/8D0D08CF-790D-4586-B726-C6469A9ED49C" },
-                { "12", "8/D/0/8D0D08CF-790D-4586-B726-C6469A9ED49C" }
+                { 42, "F/8/A/F8AF50AB-3C3A-4BC4-8773-DC27B32988DD" },
+                { 17, "F/8/A/F8AF50AB-3C3A-4BC4-8773-DC27B32988DD" },
+                { 41, "D/4/1/D417998A-58EE-4EFE-A7CC-39EF9E020768" },
+                { 16, "D/4/1/D417998A-58EE-4EFE-A7CC-39EF9E020768" },
+                { 40, "3/4/2/342316D7-EBE0-4F10-ABA2-AE8E0CDF36DD" },
+                { 15, "3/4/2/342316D7-EBE0-4F10-ABA2-AE8E0CDF36DD" },
+                { 38, "3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15" },
+                { 14, "3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15" },
+                { 5, "C/0/7/C07EBF21-5305-4EC8-83B1-A6FCC8F93F45" },
+                { 13, "C/0/7/C07EBF21-5305-4EC8-83B1-A6FCC8F93F45" },
+                { 20, "8/D/0/8D0D08CF-790D-4586-B726-C6469A9ED49C" },
+                { 12, "8/D/0/8D0D08CF-790D-4586-B726-C6469A9ED49C" }
             };
         }
 
         private static void SetupEdgeChromiumDictionary()
         {
-            VersionList = new Dictionary<string, string>
+            VersionList = new Dictionary<int, string>
             {
                 //updated June 2019
                 //source: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
@@ -248,10 +248,13 @@ namespace WebDriverManager.Helpers
                 //Version numbers represent either Edge brower and Edge HTML - due to way Microsoft display both versions
                 //both are shown, so including both to ensure users don't get an error if using either version
 
-                { "78", "78.0.246.0" },
-                { "77", "77.0.237.0" },
-                { "76", "76.0.183.0" },
-                { "75", "75.0.139.20" }
+                { 82, "82.0.418.0" },
+                { 81, "81.0.416.0" },
+                { 80, "80.0.361.48" },
+                { 78, "78.0.246.0" },
+                { 77, "77.0.237.0" },
+                { 76, "76.0.183.0" },
+                { 75, "75.0.139.20" }
             };
         }
     }
